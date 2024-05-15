af9907e241e27ba292618734caf5690d76849f37
fabi . <fabian.monar.u@gmail.com>
2024-05-15 14:54:52 -0300

    Notificacion

---
+++ index.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+        title="Alerta de prueba", 
+        message="Este es el cuerpo de la notifiacion",
+        timeout=10,
+        )
+    time.sleep(10)
+
+
+
